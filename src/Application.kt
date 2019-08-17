65116bf684bef5c0d6d7bacd3ed26eb94b9b0702
James Ho <jamesho19@gmail.com>
2019-08-17 11:05:20 +1000

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,137 @@
+package com.iotarch
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import freemarker.cache.*
+import io.ktor.auth.*
+import io.ktor.freemarker.*
+import io.ktor.http.content.*
+import io.ktor.sessions.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
+
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+
+    install(Authentication){
+        basic(name="myAuth"){
+            validate { credentials ->
+                if (credentials.name == credentials.password) {
+                    UserIdPrincipal(credentials.name)
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
+//    val client = HttpClient() {
+//    }
+
+
+
+    routing {
+
+
+        authenticate("myAuth") {
+            get("/auth") {
+                call.respond("This is authorized")
+            }
+        }
+
+
+        get("/html-dsl") {
+            call.respondHtml {
+                body {
+                    h1 { +"HTML" }
+                    ul {
+                        for (n in 1..10) {
+                            li { +"$n" }
+                        }
+                    }
+                }
+            }
+        }
+
+        get("/styles.css") {
+            call.respondCss {
+                body {
+                    backgroundColor = Color.red
+                }
+                p {
+                    fontSize = 2.em
+                }
+                rule("p.myclass") {
+                    color = Color.blue
+                }
+            }
+        }
+
+        get("/"){
+            call.respond(FreeMarkerContent("index.ftl",null))
+        }
+
+        get("/html-freemarker") {
+            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
+        }
+
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+
+        }
+
+        get("/session/increment") {
+            val session = call.sessions.get<MySession>() ?: MySession()
+            call.sessions.set(session.copy(count = session.count + 1))
+            call.respondText("Counter is ${session.count}. Refresh to increment.")
+        }
+
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+    }
+}
+
+data class IndexData(val items: List<Int>)
+
+data class MySession(val count: Int = 0)
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
+
+fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
+    style(type = ContentType.Text.CSS.toString()) {
+        +CSSBuilder().apply(builder).toString()
+    }
+}
+
+fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
+    this.style = CSSBuilder().apply(builder).toString().trim()
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
